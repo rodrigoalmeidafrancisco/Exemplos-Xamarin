09608e2635d0eb1199f32e7c200035b6e722426c
Rodrigo de Almeida Francisco <rdafrancisco@tjsp.jus.br>
2021-04-15 22:33:39 -0300

    Configurado o nuget - Xam.Plugin.Media

--- Projects/Front-End/AppExemplo.Android/Properties/AssemblyInfo.cs
+++ Projects/Front-End/AppExemplo.Android/Properties/AssemblyInfo.cs
@@ -28,3 +28,7 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+//As duas linhas abaixo, faz com que a loja Google Play filtre dispositivos sem o hardware necessário.
+[assembly: UsesFeature("android.hardware.camera", Required = true)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = true)]
---
+++ Projects/Front-End/AppExemplo/GlobalApp.cs
@@ -0,0 +1,17 @@
+﻿namespace AppExemplo
+{
+    public static class GlobalApp
+    {
+        #region Preferences
+
+        //Keys
+        public static string Preferences_Key_Login { get { return "Preferences_Login"; } }
+        public static string Preferences_Key_Name { get { return "Preferences_Login"; } }
+
+        //Shared
+        public static string Preferences_Shared_Login { get { return "SharedName_Login"; } }
+
+        #endregion Preferences
+
+    }
+}
